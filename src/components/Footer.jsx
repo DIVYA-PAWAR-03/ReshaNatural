2b4f8c2ce055ef65150c7826ca32765391040816
DIVYA-PAWAR-03 <121420835+DIVYA-PAWAR-03@users.noreply.github.com>
2025-02-24 18:12:09 +0530

    init

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,17 +20,17 @@ export default function Footer() {
              <div className="address-info">
                 <div className="first-address-sec">
                 <h4>FACTORY ADDRESS:</h4>
-                <address>Survey no.- 92 bukanwadi, village - <br /> gorewadi, tal & dist. Osmanabad, <br /> 413509, Maharashtra state, India.</address>
+                <p>Survey no.- 92 bukanwadi, village - <br /> gorewadi, tal & dist. Osmanabad, <br /> 413509, Maharashtra state, India.</p>
                 </div>
                 <div className="second-address-sec">
                 <h4>REGISTERED OFFICE:</h4>
-                <address>Ruturaj 27/89 Anand Nagar, <br /> Osmanabad, 413501, Maharashtra state,<br /> India.</address>
+                <p>Ruturaj 27/89 Anand Nagar, <br /> Osmanabad, 413501, Maharashtra state,<br /> India.</p>
                 </div>
              </div>
              <div className="correspondance-address">
              <h4>CORRESPONDANCE ADDRESS:</h4>
-                <address>1118 vidya-1, lakaki road, Model <br /> colony, Pune 411016, Maharashtra state, India. <br />
-                mobile no:+91-9422465883</address>
+                <p>1118 vidya-1, lakaki road, Model <br /> colony, Pune 411016, Maharashtra state, India. <br />
+                mobile no:+91-9422465883</p>
                 <div className="social-media-icons">
              <h4>CONNECT WITH US</h4>
               <div className="social-links">
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,7 +2,11 @@ export default function Products() {
   return (
     <main>
       <div className="product-main-container">
-        <img src="/images/product-farm-full.jpg" alt="" className="product-img" />
+        <img
+          src="/images/product-farm-full.jpg"
+          alt=""
+          className="product-img"
+        />
       </div>
 
       <div className="product-text-contaier">
@@ -15,91 +19,127 @@ export default function Products() {
           Through our innovative health product we have made an attempt to
           provide all natural dietary fiber food supplements to add to your
           routine food products or diet plan. It will help balancing your diet
-          to overcome lifestyle disease in a simplest way. <br />  <br /> Saiber-kn
-          being neutral in taste, color & odor do not alter original taste of
-          the product. Sugarcane juice is converted into jaggery (traditional
-          chemical free Indian sweetener rich in minerals).
+          to overcome lifestyle disease in a simplest way. <br /> <br />{" "}
+          Saiber-kn being neutral in taste, color & odor do not alter original
+          taste of the product. Sugarcane juice is converted into jaggery
+          (traditional chemical free Indian sweetener rich in minerals).
         </p>
       </div>
 
-
-
       <div className="product-img-container">
-        <div className="first-img-container">
-          <img src="/images/products1-new.png" alt="" className="pro-img" />
-          <img src="/images/jaggery1.jpg" alt="" className="pro-img" />
+        <div className="img-container">
+          <div className="first-img-container">
+            <img src="/images/jaggery.jpg" alt="" className="pro-img1" />
+          </div>
         </div>
-        <div className="second-img-container">
-          <img src="/images/jaggery1.jpg" alt="" className="pro-img" />
-          <img src="/images/products1-new.png" alt="" className="pro-img" />
+
+        <div className="img-container">
+          <div className="second-img-container">
+            <img src="/images/productsForcontainer.png" alt="" className="pro-img2" />
+          </div>
+        </div>
+        <div className="img-container">
+          <div className="third-img-container">
+            <img src="/images/jaggery.jpg" alt="" className="pro-img3" />
+          </div>
+        </div>
+        <div className="img-container">
+          <div className="fourth-img-container">
+            <img src="/images/productsForcontainer.png" alt="" className="pro-img4" />
+          </div>
         </div>
       </div>
-      
-
 
       <div className="product-description">
         <div className="product-desc-img">
-            <img src="/images/products1.png" alt="" />
+          <img src="/images/products1.png" alt="" />
         </div>
         <div className="product-desc-one">
-            <h2>SAIFER - KN :-</h2>
-            <h3>Natural dietary fiber from sugarcane by innovative process</h3>
-            <p>Saifer-kn is a fine flour or powder that can be incorporated into almost all foods products to increase yields, shelf life, texture and mouth-feel without any change in taste or flavor. The ratio of addition of Saifer-kn may vary from 5 to 15 % depending on type of food product and characteristics expected.</p>
-            <h3>SAIFER - kn is Fundamentally</h3>
-            <p>Food fiber, extracted from natural plants. All plants cellulose, natural sugar polymer as a fundamental framework. Chemically cellulose is a polysaccharide, complex carbohydrate molecules with fully natural polymer, three dimensional fiber network. Fundamental part of every plant, purified by cleaning processes of plants which is neutral in taste and odor, non digestible by human enzyme system.</p>
-            <h3>Benefit for food product / food manufacturer</h3>
-            <ul>
-                <li>Added nutritional value</li>
-                <li>Increases shelf life</li>
-                <li>Increased yield, adds volume to product</li>
-                <li>Maintains moisture</li>
-                <li>Improves texture in baked items, Juiciness to meat products</li>
-                <li>Do not alter taste & flavor of food product</li>
-                <li>Stable to heat and freezing conditions</li>
-                <li>Natural, Easy to use</li>
-                <li>Cost saving</li>
-            </ul>
+          <h2>SAIFER - KN :-</h2>
+          <h3>Natural dietary fiber from sugarcane by innovative process</h3>
+          <p>
+            Saifer-kn is a fine flour or powder that can be incorporated into
+            almost all foods products to increase yields, shelf life, texture
+            and mouth-feel without any change in taste or flavor. The ratio of
+            addition of Saifer-kn may vary from 5 to 15 % depending on type of
+            food product and characteristics expected.
+          </p>
+          <h3>SAIFER - kn is Fundamentally</h3>
+          <p>
+            Food fiber, extracted from natural plants. All plants cellulose,
+            natural sugar polymer as a fundamental framework. Chemically
+            cellulose is a polysaccharide, complex carbohydrate molecules with
+            fully natural polymer, three dimensional fiber network. Fundamental
+            part of every plant, purified by cleaning processes of plants which
+            is neutral in taste and odor, non digestible by human enzyme system.
+          </p>
+          <h3>Benefit for food product / food manufacturer</h3>
+          <ul>
+            <li>Added nutritional value</li>
+            <li>Increases shelf life</li>
+            <li>Increased yield, adds volume to product</li>
+            <li>Maintains moisture</li>
+            <li>Improves texture in baked items, Juiciness to meat products</li>
+            <li>Do not alter taste & flavor of food product</li>
+            <li>Stable to heat and freezing conditions</li>
+            <li>Natural, Easy to use</li>
+            <li>Cost saving</li>
+          </ul>
 
-            <h3>Product features -</h3>
-            <ul>
-                <li>100% natural dietary fiber</li>
-                <li>Very high fiber claim</li>
-                <li>Glutton free, Prebiotech</li>
-                <li>Green label, no chemical alteration process</li>
-                <li>Non GMO</li>
-                <li>Made in India, country of origin of sugarcane & cane processing technology</li>
-            </ul>
+          <h3>Product features -</h3>
+          <ul>
+            <li>100% natural dietary fiber</li>
+            <li>Very high fiber claim</li>
+            <li>Glutton free, Prebiotech</li>
+            <li>Green label, no chemical alteration process</li>
+            <li>Non GMO</li>
+            <li>
+              Made in India, country of origin of sugarcane & cane processing
+              technology
+            </li>
+          </ul>
         </div>
       </div>
       <div className="gaggery-desc">
         <div className="gaggery-desc-section">
-             <h2>JAGGERY:-</h2>
-             <h3>Natural sweetener & Healthier substitute for sugar</h3>
-             <p>Jaggery is a traditional non-centrifugal unrefined sugar consumed mainly in Asia and Africa. It is a concentrated product of cane juice without separation of the molasses and crystals. It contains up to 50% sucrose, up to 20% invert sugars, and up to 20% moisture. Jaggery is a traditional sweetener in Indian subcontinent originated in India. It is more natural & healthier substitute for sugar. <br />
-             Ayurveda has used jaggery to treat various ailments as it is rich in iron, vitamins and other minerals. jaggery is also rich in magnesium, potassium, manganese and zinc which it retains because it doesn’t go through extensive processing.</p>
-             <h3>Product Features -</h3>
-             <ul>
-                <li>Rich in iron, zinc , vitamins & minerals</li>
-                <li>Chemical less manufacturing process</li>
-                <li>Recommended for medicinal use in Ayurveda <br /> ( traditional Indian medical science)</li>
-                <li>Traditional sweetener popular in Indian subcontinent</li>
-                <li>Unrefined, manufactured excessive processing</li>
-                <li>Adds test & flavor to the product</li>
-             </ul>
+          <h2>JAGGERY:-</h2>
+          <h3>Natural sweetener & Healthier substitute for sugar</h3>
+          <p>
+            Jaggery is a traditional non-centrifugal unrefined sugar consumed
+            mainly in Asia and Africa. It is a concentrated product of cane
+            juice without separation of the molasses and crystals. It contains
+            up to 50% sucrose, up to 20% invert sugars, and up to 20% moisture.
+            Jaggery is a traditional sweetener in Indian subcontinent originated
+            in India. It is more natural & healthier substitute for sugar.{" "}
+            <br />
+            Ayurveda has used jaggery to treat various ailments as it is rich in
+            iron, vitamins and other minerals. jaggery is also rich in
+            magnesium, potassium, manganese and zinc which it retains because it
+            doesn’t go through extensive processing.
+          </p>
+          <h3>Product Features -</h3>
+          <ul>
+            <li>Rich in iron, zinc , vitamins & minerals</li>
+            <li>Chemical less manufacturing process</li>
+            <li>
+              Recommended for medicinal use in Ayurveda <br /> ( traditional
+              Indian medical science)
+            </li>
+            <li>Traditional sweetener popular in Indian subcontinent</li>
+            <li>Unrefined, manufactured excessive processing</li>
+            <li>Adds test & flavor to the product</li>
+          </ul>
         </div>
         <div className="gaggery-img">
-            <img src="/images/jaggery.jpg" alt="" />
+          <img src="/images/jaggery.jpg" alt="" />
         </div>
       </div>
-      
-
 
       <div className="products-icons">
         <i className="fa-regular fa-heart like"></i>
         <i className="fa-brands fa-twitter"></i>
         <i className="fa-brands fa-facebook"></i>
-
-        </div>
-      </main>
+      </div>
+    </main>
   );
 }
